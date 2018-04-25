8f68b99f728b2027a15740ce897a7ce30463b8af
Joy <joyterra@gmail.com>
2018-04-25 10:47:17 +0200

    yes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Form from './components/Form';
 import Weather from './components/Weather';
 
 const API_KEY = "6e806dcb4c33c4decf89fb57ed9729b9";
-// const API_URL = "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=6e806dcb4c33c4decf89fb57ed9729b9"
+// const API_URL = "https://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=6e806dcb4c33c4decf89fb57ed9729b9"
 
 class App extends Component {
   state = {
@@ -20,7 +20,7 @@ class App extends Component {
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
 
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
+    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
     const data = await api_call.json();
      if (city && country){
       this.setState({
